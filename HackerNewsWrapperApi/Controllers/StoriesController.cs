f381ada6634ef72c0b8498c472736729b4d639fd
Коля Череватов <nounem@outlook.com>
2023-09-22 00:55:04 +0300

    implement best-stories endpoint

--- HackerNewsWrapperApi/Controllers/StoriesController.cs
+++ HackerNewsWrapperApi/Controllers/StoriesController.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace HackerNewsWrapperApi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class StoriesController : ControllerBase
 {
-    
-    
-    
+    [HttpGet("best-stories")]
+    public async Task<ActionResult<IEnumerable<object>>> BestStoriesAsync(int storiesCount)
+    {
+        return Ok();
+    }
 }
