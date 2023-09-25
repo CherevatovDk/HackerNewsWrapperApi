4171851577f5e05a119161dcc52fbd089f8c4825
Коля Череватов <nounem@outlook.com>
2023-09-25 23:55:59 +0300

    create service and cache

--- HackerNewsWrapperApi/Controllers/StoriesController.cs
+++ HackerNewsWrapperApi/Controllers/StoriesController.cs
@@ -1,4 +1,5 @@
 using HackerNewsWrapperApi.Dtos;
+using HackerNewsWrapperApi.Interface;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HackerNewsWrapperApi.Controllers;
@@ -7,9 +8,18 @@ namespace HackerNewsWrapperApi.Controllers;
 [Route("api/[controller]")]
 public class StoriesController : ControllerBase
 {
+    private readonly IHackerHttpService _hackerHttpService;
+
+    public StoriesController(IHackerHttpService hackreHttpService)
+    {
+
+        _hackerHttpService = hackreHttpService;
+    }
+
     [HttpGet("best-stories")]
     public async Task<ActionResult<IEnumerable<StoryDto>>> BestStoriesAsync(int storiesCount)
     {
-        return Ok();
+
+        return Ok(await _hackerHttpService.GetStorie(storiesCount));
     }
 }
--- HackerNewsWrapperApi/Program.cs
+++ HackerNewsWrapperApi/Program.cs
@@ -1,6 +1,12 @@
+using HackerNewsWrapperApi.Interface;
+using HackerNewsWrapperApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient();
+builder.Services.AddMemoryCache();
+builder.Services.AddScoped<IHackerHttpService,HackerHttpService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -9,6 +15,8 @@ builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
