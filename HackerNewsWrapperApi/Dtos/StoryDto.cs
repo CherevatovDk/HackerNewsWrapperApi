bbf0740b4d7d239d875cd5d31dae4a589684fa50
Коля Череватов <nounem@outlook.com>
2023-09-26 17:20:06 +0300

    add models

---
+++ HackerNewsWrapperApi/Dtos/StoryDto.cs
@@ -0,0 +1,24 @@
+using Newtonsoft.Json;
+
+namespace HackerNewsWrapperApi.Dtos;
+
+public class StoryDto
+{
+    [JsonProperty("title")]
+    public string Title { get; set; }
+    
+    [JsonProperty("uri")]
+    public string Uri { get; set; }
+    
+    [JsonProperty("postedBy")]
+    public string PostedBy { get; set; }
+    
+    [JsonProperty("time")]
+    public DateTime Time { get; set; }
+
+    [JsonProperty("score")]
+    public int Score { get; set; }
+
+    [JsonProperty("commentCount")]
+    public int CommentCount { get; set; }
+}
