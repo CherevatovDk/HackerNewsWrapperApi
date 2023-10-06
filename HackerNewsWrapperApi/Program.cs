81d4aa8fc7d414d4028009c0251aa6b5bdae248a
Коля Череватов <nounem@outlook.com>
2023-10-06 20:07:47 +0300

    create service and cache for first url

--- HackerNewsWrapperApi/Program.cs
+++ HackerNewsWrapperApi/Program.cs
@@ -2,13 +2,9 @@ using HackerNewsWrapperApi.Interfaces;
 using HackerNewsWrapperApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-var configuration = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json")
-    .Build();
+new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,8 +12,6 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddMemoryCache();
 builder.Services.AddScoped<HttpClient>();
 builder.Services.AddScoped<IHackerHttpService, HackerHttpService>();
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,9 +22,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
